8f18e63c3a6a3f62a923727f34100fd644bfa9af
triptishergill <triptishergill@gmail.com>
2015-10-15 16:06:00 +0530

    Second Maven

---
+++ src/test/java/com/learntestng/AppTest.java
@@ -0,0 +1,17 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void sendMail()
+    {
+    	System.out.println("checked");
+    }
+    
+}
---
+++ src/test/java/com/learntestng/gene.java
@@ -0,0 +1,14 @@
+package com.learntestng;
+
+import org.testng.annotations.Test;
+
+public class gene {
+	
+	 @Test
+	    public void edfvv123()
+	    {
+	    	System.out.println("***********checked2************");
+	    }
+	    
+
+}
